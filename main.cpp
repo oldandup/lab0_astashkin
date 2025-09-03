2ddff098a5924803f14b16b402608b7a19c838aa
oldandup <denes2006@mail.ru>
2025-09-03 22:22:43 +0300

    Task done! (second try)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main() {
+    std::string user_symbol;
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Enter any sybmols: ";
+    std::cin >> user_symbol;
+    std::cout << "Your symbols is:" << user_symbol;
     return 0;
 }
